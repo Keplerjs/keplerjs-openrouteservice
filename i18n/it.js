9a4c72aeba5e6385875f8ab404a99d7901ca5392
stefanocudini <stefano.cudini@gmail.com>
2019-03-19 14:12:39 +0100

    i18n it

---
+++ i18n/it.js
@@ -0,0 +1,10 @@
+
+i18n.map('it', {
+	label_ors_profiles: "Profilo Indicazioni",
+
+	btn_ors_directions: "Indicazioni...",
+	btn_ors_from: "Da qui",
+	btn_ors_to: "a qui",
+
+	error_ors_directions_to: "Seleziona una destinazione sulla mappa"
+});
